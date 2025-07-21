8c5244e275fb0ebc1a07457a22ba96063a11d8d8
root <root@LAPTOP-DT0TUA10.localdomain>
2025-07-21 17:23:07 +0700

    🔥 fresh from the oven

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            ClubSeeder::class,
-            PlayerSeeder::class,
         ]);
     }
 }
